74591d3b9bf90768ccccb723ae8414523cbb12e9
ashdhaker <ashdhaker09@gmail.com>
2023-03-28 19:49:31 +0530

    push

---
+++ src/components/Tech.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
+
+const Tech = () => {
+  return (
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      <h1 class='mt-4 text-secondary text-[35px] max-w-3xl leading-[30px]'>SKILLS</h1>
+      {technologies.map((technology) => (
+        <div className='w-28 h-28' key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SectionWrapper(Tech, "");
---
+++ src/constants/index.js
@@ -0,0 +1,246 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Flutter Developer",
+    icon: backend,
+  },
+  {
+    title: "quick learner",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "git",
+    icon: git,
+  }
+];
+
+const experiences = [
+  {
+    title: "React.js Developer",
+    company_name: "MNNIT",
+    icon: starbucks,
+    iconBg: "#383E56",
+    date: "March 2023 - continue",
+    points: [
+      "Developing web applications using React.js and other related technologies.",
+      // "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      // "Implementing responsive design and ensuring cross-browser compatibility.",
+      // "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "full stack Developer",
+    company_name: "MNNIT",
+    icon: tesla,
+    iconBg: "#E6DEDD",
+    date: "Jan 2021 - Feb 2023",
+    points: [
+      "Developing server side web applications using node.js and other related technologies.",
+    ],
+  },
+  {
+    title: "BTech CSE",
+    company_name: "MNNIT",
+    icon: shopify,
+    iconBg: "#383E56",
+    date: "Jan 2020 - Jan 2024",
+    points: [
+      "currently in 6 sem with cpi 8.21",
+      "Motilal Nehru National Institute of Technology Allahabad, formerly Motilal Nehru Regional Engineering College, is one of the 31 National Institutes of Technology, located in Allahabad, Uttar Pradesh, India",
+      // "Developing and maintaining web applications using React.js and other related technologies.",
+      // "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      // "Implementing responsive design and ensuring cross-browser compatibility.",
+      // "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "10+2 CBSE",
+    company_name: "Manglam DAV public school",
+    icon: meta,
+    iconBg: "#E6DEDD",
+    date: "Jan 2017 - 2020",
+    points: [
+      "got 85% in 12th board",
+      "got 10cpi in 10th board"
+      // "Developing and maintaining web applications using React.js and other related technologies.",
+      // "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      // "Implementing responsive design and ensuring cross-browser compatibility.",
+      // "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Click Bay",
+    description:
+      "",
+    tags: [
+      {
+        name: "flutter",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "dart",
+        color: "green-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/ashdhaker/ClickBay",
+  },
+  {
+    name: "Fitness App",
+    description:
+      "",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Daily Journal",
+    description:
+      "",
+    tags: [
+      {
+        name: "nodejs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "expressjs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "html",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "css",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/ashdhaker/Daily-Journal",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
